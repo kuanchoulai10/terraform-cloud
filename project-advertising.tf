ae564399d3f7c90f7ec1501aab0dd5dfb5cb8aee
kuanchoulai10 <edisonlai@innov.tvbs.com.tw>
2023-06-20 23:59:12 +0800

    Create advertising project and workspace

---
+++ project-advertising.tf
@@ -0,0 +1,34 @@
+resource "tfe_project" "advertising" {
+  organization = data.tfe_organization.kcl.name
+  name = "Advertising"
+}
+
+resource "tfe_workspace" "advertising_data_pipeline" {
+  organization        = tfe_organization.main.name
+  name                = "Advertising Data Pipeline"
+  project_id          = tfe_project.advertising.id
+  execution_mode      = "remote"
+  auto_apply          = false
+  terraform_version   = "~> 1.5.0"
+  working_directory   = "advertising/infrastructure/"
+  global_remote_state = false
+  remote_state_consumer_ids = [
+  ]
+  structured_run_output_enabled = true
+  tag_names = [
+    "department:advertising"
+  ]
+
+  speculative_enabled = true
+  vcs_repo {
+    identifier         = "kuanchoulai10/data-mesh"
+    branch             = "main"
+    ingress_submodules = false
+    oauth_token_id     = data.tfe_github_app_installation.main.id
+  }
+
+  # Other settings
+  queue_all_runs      = false
+  assessments_enabled = false
+  allow_destroy_plan  = false
+}
